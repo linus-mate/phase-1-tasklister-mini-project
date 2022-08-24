4bf29d35299a845ff047a8840444515be73f6f0d
linus mutugi mate <mlynux@gmail.com>
2022-08-24 12:31:33 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toDoList(e.target.querySelector('#new-task-description').value);
+    form.reset()
+  })
 });
+
+
+function toDoList(lists){
+  let li = document.createElement ("li")
+  li.textContent = `{lists}`
+  document.querySelector('#tasks').appendChild(li)
+
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  li.appendChild(btn)
+
+  btn.addEventListener('click', handleDelete)
+  
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
